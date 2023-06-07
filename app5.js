9d6cfb89f98c8c7c5e1ea7516d78d717a58bc91e
Marcos Gentili <Marcosgentili66@gmail.com>
2023-06-07 20:29:49 -0300

    Second

---
+++ app5.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+function calcularMedia (nota1,nota2,nota3) {
+    let media = (nota1+nota2+nota3)/3
+    console.log(media)
+}
+
+calcularMedia(10,8,7)
+calcularMedia(10,5,2)
+calcularMedia(20/20*0.3/12,5,2)
